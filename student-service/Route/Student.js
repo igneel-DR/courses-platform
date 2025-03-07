df4b5728b51e71ffbd9e1d356f72ca55fb5a8461
Hicham <bleus332@gmail.com>
2025-03-07 17:29:59 +0000

    first commit

--- student-service/Route/Student.js
+++ student-service/Route/Student.js
@@ -5,6 +5,10 @@ const StudentModel = require('../Model/StudentModel')
 const axios = require("axios")
 const {verifyToken} = require('../Middleware/Auth')
 
+function findDeletedCourses(existingIds, enrolledIds) {
+    return enrolledIds.filter(id => !existingIds.includes(id));
+}
+
 router.get("/all", verifyToken, async(req, res)=>{
     try{
         const Students = await StudentModel.find({})
@@ -54,23 +58,36 @@ router.post("/enroll/:etudiant_id/:cours_id", verifyToken, async(req, res)=>{
     }
 })
 
-router.get("/enrolledCourses/:etudiant_id", async(req, res)=>{
-    try{
-        const Student = await StudentModel.find({id: req.params.etudiant_id})
-        if (!Student){
-            return res.json({message: "student not found"}) 
+router.get("/enrolledCourses/:etudiant_id", verifyToken, async (req, res) => {
+    try {
+        const student = await StudentModel.findOne({ id: req.params.etudiant_id });
+        if (!student) {
+            return res.status(404).json({ message: "Student not found" });
         }
 
-        const fetchCourses = await axios.get("http://localhost:3001/course/all");
+        const enrolledCourseIds = student.cours; // student courses ids
+
+        const fetchCourses = await axios.get('http://localhost:3001/course/all',{
+            headers: { 
+                'Authorization': req.headers['authorization'] 
+            }
+        });
+
         const allCourses = fetchCourses.data;
+        const existingCourseIds = allCourses.map(course => course.id); // course ids
 
-        const enrolledCourses = allCourses.filter(course => Student.cours.includes(course.id));
+        const deletedCourses = findDeletedCourses(existingCourseIds, enrolledCourseIds);
+        if(deletedCourses){
+            await StudentModel.updateOne({ id: req.params.etudiant_id }, { $pull: { cours: { $in: deletedCourses } } });
+        } 
 
-        res.json(enrolledCourses)
+        const studentCourses = allCourses.filter(course => enrolledCourseIds.includes(course.id));
+
+        res.status(200).json(studentCourses);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-    catch (error) {
-        res.status(500).json({message: error.message});
-    }
-})
+});
+
 
 module.exports = router;
